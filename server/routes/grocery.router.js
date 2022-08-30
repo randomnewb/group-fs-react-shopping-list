bfdbf2490befa24e48ca8ec36aa32920b1332764
Nathan Gaede <n.d.gaede@gmail.com>
2022-08-29 20:45:02 -0500

    Create GET route and logged it to the DOM

--- server/routes/grocery.router.js
+++ server/routes/grocery.router.js
@@ -2,4 +2,26 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool.js");
 
+router.get('/', (req, res) => {
+    console.log("In GET request");
+    let queryText = 'SELECT * from "grocery"';
+
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+});
+
+//router.post 
+
+
+
+
+
+
+
+
+
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import Header from "../Header/Header.jsx";
 import Input from "../Input/Input.jsx";
@@ -7,9 +7,32 @@ import Item from "../Item/Item.jsx";
 import "./App.css";
 
 function App() {
+    //Todo Axios GET to fetch items and console.log grocery items
+    let [groceryArray, setGroceryArray] = useState([]);
+
+  // TODO: on load, call the fetchPeople() function
+  useEffect(() => {
+    console.log('useEffect - page load');
+    fetchGroceries();
+  }, []);
+
+  const fetchGroceries = () => {
+    // TODO: fetch the list of people from the server
+    axios({
+      method: 'GET',
+      url: '/grocery',
+    }).then(response => {
+      setGroceryArray(response.data);
+    }).catch(error => {
+      console.log(error);
+      alert('Something went wrong');
+    });
+  }
     return (
         <div className="App">
             <Header />
+            {/* Log to the DOM */}
+            {JSON.stringify(groceryArray)}
             <main>
                 <p>Under Construction...</p>
             </main>
